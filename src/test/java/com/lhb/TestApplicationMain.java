5e9626cb6c107c8d74ff88dcac5d9ba263186493
lihaobin <2396084146@qq.com>
2022-09-21 20:44:08 +0800

    完成测试2

--- src/test/java/com/lhb/TestApplicationMain.java
+++ src/test/java/com/lhb/TestApplicationMain.java
@@ -8,15 +8,29 @@ import java.util.List;
 import java.util.Map;
 
 public class TestApplicationMain {
+
+    private static final String path = "orig.txt";
+    private static final String outPath = "C:\\Users\\86155\\Desktop\\大三上\\软件工程\\";
+
     //相同性测试
     @Test
     public void testSame(){
-        String path = "orig.txt";
         String path2 = "orig_add.txt";
-        Map<String, List<Integer>> stringListMap1 = TokenizerUtil.CountWord ( path );
+        Map<String, List<Integer>> stringListMap1 = TokenizerUtil.CountWord ( this.path );
         Map<String, List<Integer>> stringListMap2 = TokenizerUtil.CountWord ( path2 );
         Double similarity = TokenizerUtil.CosCount ( stringListMap1,stringListMap2 );
-        FileUtil.writeFile ( "C:\\Users\\86155\\Desktop\\大三上\\软件工程\\res.txt",String.valueOf ( similarity ) );
+        FileUtil.writeFile ( this.outPath+"res.txt",String.valueOf ( similarity ) );
+        System.out.println (similarity );
+    }
+
+    //原先文本和增加文本的对比
+    @Test
+    public void testAdd(){
+        String path2 = "orig_add_2.txt";
+        Map<String, List<Integer>> stringListMap1 = TokenizerUtil.CountWord ( this.path );
+        Map<String, List<Integer>> stringListMap2 = TokenizerUtil.CountWord ( path2 );
+        Double similarity = TokenizerUtil.CosCount ( stringListMap1,stringListMap2 );
+        FileUtil.writeFile ( this.outPath+"res2.txt",String.valueOf ( similarity ) );
         System.out.println (similarity );
     }
 }
